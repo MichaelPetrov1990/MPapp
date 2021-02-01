2ae5de31be53bbd23d244fad407be8be0d50217b
Michael Petrov <mpetrovcello@gmail.com>
2021-02-01 17:07:48 +0000

    changes after our call with Tyrone

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+questionnaire1 = Questionnaire.create(category: "Health", question1: "From 1 to 10, please rate your fintness", question2: "Again please rate your mental health", question3: "Rate your nutrition")
+questionnaire2 = Questionnaire.create(category: "Happiness", question1: "Rate your relationships", question2: "Rate your pleaure", question3: "Rate your interests")
+questionnaire3 = Questionnaire.create(category: "Wealth", question1: "Rate your finances", question2: "Rate your social life", question3: "How well do you use your time?")
