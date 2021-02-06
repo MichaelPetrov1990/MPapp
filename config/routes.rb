c19a6f909d2229f9737a7b0c3e4808cfc6c720de
Michael Petrov <mpetrovcello@gmail.com>
2021-02-06 12:21:50 +0000

    debugging

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'faqs', to: 'pages#faqs'
-  resources :questionnaire do
-    resources :question do
-      resources :answer
+  resources :questionnaires do
+    resources :questions do
+      resources :answers
     end
   end
 
