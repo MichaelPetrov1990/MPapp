437dfa6afa23f2d853f85dc743ace49eeb059bb1
Tarun Mookhey <tarun@pacificasearch.com>
2021-02-07 17:49:12 +0530

    updated seed file, added question association, added answer association, added scenarios as comments to answer.rnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-questionnaire1 = Questionnaire.create(category: "Health", question1: "From 1 to 10, please rate your fintness", question2: "Again please rate your mental health", question3: "Rate your nutrition")
-questionnaire2 = Questionnaire.create(category: "Happiness", question1: "Rate your relationships", question2: "Rate your pleaure", question3: "Rate your interests")
-questionnaire3 = Questionnaire.create(category: "Wealth", question1: "Rate your finances", question2: "Rate your social life", question3: "How well do you use your time?")
+
+Questionnaire.destroy_all
+
+questionnaire1 = Questionnaire.create(category: "Health")
+question1 = questionnaire1.questions.create(body: "From 1 to 10, please rate your fitness")
+question2 = questionnaire1.questions.create(body: "Again please rate your physical health")
+question3 = questionnaire1.questions.create(body: "Rate your nutrition")
+
+questionnaire1 = Questionnaire.create(category: "Wealth")
+question1 = questionnaire1.questions.create(body: "From 1 to 10, please rate your financial fitness")
+question2 = questionnaire1.questions.create(body: "Again please rate your financial health")
+question3 = questionnaire1.questions.create(body: "Rate your financial nutrition")
+
+questionnaire1 = Questionnaire.create(category: "Happiness")
+question1 = questionnaire1.questions.create(body: "From 1 to 10, please rate your happy fitness")
+question2 = questionnaire1.questions.create(body: "Again please rate your happy health")
+question3 = questionnaire1.questions.create(body: "Rate your happy nutrition")
