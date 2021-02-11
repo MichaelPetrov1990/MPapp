17dc48da39efa90f99e2e2f53b660712ba5c8f56
Tarun Mookhey <tarun@pacificasearch.com>
2021-02-11 17:13:36 +0530

    added questionnaire routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get 'questionnaire/:user_id', to: 'questionnaires#show', as: 'questionnaire'
+  post 'questionnaire/:id', to: 'questionnaires#create'
+
   get 'about', to: 'pages#about'
   get 'faqs', to: 'pages#faqs'
-  resources :questionnaires do
-    resources :questions do
-      resources :answers 
-    end
-  end
-  #get	'/questions/:question_id/answers/new',	to: 'answers#new' 
-  #resources :answers, only: [ :new, :create ]
-    
-
-
-  resources :plan, only: [:show, :new, :create, :edit, :update]
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+ end
