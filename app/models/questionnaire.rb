6ee3a45547d28e60bf78d5569efbf10aa5e52616
Michael Petrov <mpetrovcello@gmail.com>
2021-02-24 17:30:08 +0000

    added methods for calculations of low high nad average score

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -2,4 +2,17 @@ class Questionnaire < ApplicationRecord
   belongs_to :user
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+
+  def lowest_score
+    self.answers.pluck(:rating).min
+  end
+
+  def highest_score
+    self.answers.pluck(:rating).max
+  end
+
+  def average_score
+    rating_array = self.answers.pluck(:rating)
+    rating_array.sum / rating_array.length
+  end
 end
