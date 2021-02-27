09f478f99d5b9d756f9b8cbd31182db3f73233ee
Tarun Mookhey <tarun@pacificasearch.com>
2021-02-27 16:55:09 +0530

    updated routes and path for new_questionnaires

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'questionnaires', to: 'questionnaires#index', as: "questionnaire_categories"
-  get 'questionnaire/:category', to: 'questionnaires#new', as: "new_questionnaire"
-  post 'questionnaire/:id', to: 'answers#create'
+  get 'questionnaire/category/:category', to: 'questionnaires#new', as: "new_questionnaire"
+  post 'questionnaire/:category/:id', to: 'answers#create'
   # get 'questionnaire/:id', to: 'questionnaires#show', as: 'questionnaire'
   
 
