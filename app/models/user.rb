837d6bee068b332963cb2a85d701dcb3032c7303
Tarun Mookhey <tarun@pacificasearch.com>
2021-03-02 15:55:59 +0530

    fixed user model indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
-class User < ApplicationRecord
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :questionnaires, dependent: :destroy 
-  has_many :answers
- def full_name
-   "#{first_name} #{last_name}"
- end
-end
+  class User < ApplicationRecord
+    
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
+    has_many :questionnaires, dependent: :destroy 
+    has_many :answers
+
+   def full_name
+     "#{first_name} #{last_name}"
+   end
+  end
