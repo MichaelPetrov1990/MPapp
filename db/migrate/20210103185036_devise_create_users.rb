0ce6a5c4f65ced65431ee3dfd72100a232f429f2
Michael Petrov <mpetrovcello@gmail.com>
2021-03-04 15:04:12 +0000

    added admin to user and created ali

--- db/migrate/20210103185036_devise_create_users.rb
+++ db/migrate/20210103185036_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name, null: false,  default: ""
       t.string :last_name, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.boolean :admin, null: false, default: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
