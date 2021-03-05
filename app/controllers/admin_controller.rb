1c8352c1fa3e15ced1040d3a556791785790b074
Michael Petrov <mpetrovcello@gmail.com>
2021-03-05 09:27:21 +0000

    controller queries and needed variables only

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,9 @@
 class AdminController < ApplicationController
   def questions
-    
+    @questions = Question.all
+    @health_questions = Question.where(category: "health").order(sub_category: :desc)
+    @wealth_questions = Question.where(category: "wealth").order(sub_category: :desc)
+    @happiness_questions = Question.where(category: "happiness").order(sub_category: :desc)
   end
 
   def users
