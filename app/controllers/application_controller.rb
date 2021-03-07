fc65f6f621e5fa395a6f9361153abde383f5a1fc
Tarun Mookhey <tarun@pacificasearch.com>
2021-03-08 02:03:07 +0530

    redirect from admin page unless user is admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def check_if_user_is_admin
+    redirect_to root_path, flash: {:alert => "Sorry you dont have permission  to view that page"} unless current_user && current_user.is_admin?   
+  end
 end
