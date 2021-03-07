2773d4e6e7a72528c8c5e8de5581dfc42f2a4f57
Tarun Mookhey <tarun@pacificasearch.com>
2021-03-08 02:02:29 +0530

    CRUD admin questions

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,27 @@
 class AdminController < ApplicationController
+    before_action :check_if_user_is_admin
+
   def questions
-    @questions = Question.all
-    @health_questions = Question.where(category: "health").order(sub_category: :desc)
-    @wealth_questions = Question.where(category: "wealth").order(sub_category: :desc)
-    @happiness_questions = Question.where(category: "happiness").order(sub_category: :desc)
+    @questions = Question.order(updated_at: :desc)
+  end
+
+  def create
+    @question = Question.create! "category"=>params[:category], "sub_category" => params[:sub_category], "body" => params[:body]
+    flash[:alert] = "New Question saved!"
+    redirect_to admin_questions_path
+  end
+
+  def question
+    @question = Question.find params[:id]
+    case params[:method]
+    when "update"
+      @question.update! body: params[:body], category: params[:category]
+      flash[:alert] = "Question ##{@question.id} updated!"
+    when "delete"
+      @question.destroy!
+      flash[:alert] = "Question ##{@question.id} deleted!"
+    end
+    redirect_to admin_questions_path
   end
 
   def users
