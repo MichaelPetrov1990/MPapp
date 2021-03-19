46c1371a6cc1a0f5f663d55b9b669a60f24cf629
Tarun Mookhey <tarun@pacificasearch.com>
2021-03-19 12:47:33 +0530

    updated spec_helpers file

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 RSpec.describe PagesController, type: :controller do
-  it "responds successfuly" do
+  it "loads home_page with success" do
     get :home
     expect(response.successful?).to eq(true)
   end
-  it "responds successfuly" do
+  it "loads about_page with success" do
     get :about
     expect(response.successful?).to eq(true)
   end
