f14948e3b1e8f9c93ef0cd68bd3bae2afe522512
Tarun Mookhey <tarun@pacificasearch.com>
2021-03-19 22:31:42 +0530

    temporarily disable score validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :questionnaire
   has_one :importance
-  validates :score, inclusion: { in: 1..5 }
+  # validates :score, inclusion: { in: 1..5 }
 end
