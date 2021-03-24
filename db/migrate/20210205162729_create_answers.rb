3a9d329d10d52a7f4dcc94a15fc4b82a165ae062
Michael Petrov <mpetrovcello@gmail.com>
2021-03-24 12:16:39 +0000

    weight added

--- db/migrate/20210205162729_create_answers.rb
+++ db/migrate/20210205162729_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.integer :rating
+      t.integer :weight, default: 0
       t.references :user, null: false, foreign_key: true
       t.references :questionnaire, null: false, foreign_key: true      
       t.references :question, foreign_key: true
