f64bdf84871f05ba2c2e10c90c37653374dbf06c
Michael Petrov <mpetrovcello@gmail.com>
2021-03-26 15:22:40 +0000

    added the queries as comments and the embryo is still alive

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,7 @@
 class PlansController < ApplicationController
   def show
-
+   # @rating_and_answer_id = q.answers.pluck(:rating, :id).sort #gives us an array of arrays with each being rating and answer_id
+   # rating_and_answer_id.map {|array| puts "Rating: #{array[0]} for subcategory: #{Question.find(Answer.last.question_id).sub_category}"}
   end
+  
 end
