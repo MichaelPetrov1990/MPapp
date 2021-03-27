2456c772bc3f75d1394d549f1823da2c89c9ca6a
Tarun Mookhey <tarun@pacificasearch.com>
2021-03-27 18:22:15 +0530

    fixed method

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,7 +1,8 @@
 class PlansController < ApplicationController
   def show
-   # @rating_and_answer_id = q.answers.pluck(:rating, :id).sort #gives us an array of arrays with each being rating and answer_id
-   # rating_and_answer_id.map {|array| puts "Rating: #{array[0]} for subcategory: #{Question.find(Answer.last.question_id).sub_category}"}
+   list = q.answers.pluck(:rating, :question_id).sort.take(9) #gives us an array of arrays with each being rating and answer_id
+   @rating_and_answer_id = []
+   list.map { |answer| @rating_and_answer_id << "#{Question.find(answer[1]).sub_category}:#{answer[0]}" }
   end
   
 end
