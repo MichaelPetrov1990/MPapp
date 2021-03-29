d43a2501df6847a4557e83c035e8b49088a9df76
Michael Petrov <mpetrovcello@gmail.com>
2021-03-29 10:21:48 +0100

    replaced q with query and made loop multi-line

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,8 +1,10 @@
 class PlansController < ApplicationController
   def show
-   list = q.answers.pluck(:rating, :question_id).sort.take(9) #gives us an array of arrays with each being rating and answer_id
+   list = Questionnaire.last.answers.pluck(:rating, :question_id).sort.take(9) #gives us an array of arrays with each being rating and answer_id
    @rating_and_answer_id = []
-   list.map { |answer| @rating_and_answer_id << "#{Question.find(answer[1]).sub_category}:#{answer[0]}" }
+   list.map do |answer|
+     @rating_and_answer_id.push("#{Question.find(answer[1]).sub_category}:#{answer[0]}")
+   end
   end
   
 end
