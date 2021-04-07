6e1b11a7251cf55d889574dae0e07166fc397a1d
Michael Petrov <mpetrovcello@gmail.com>
2021-04-07 11:05:30 +0100

    add welcome method witj url commented out as we haven't pushed to heroku yet

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'petrov.powerof@gmail.com'
+
+  def welcome
+    @user = params[:user]
+    # @url = https://the-power-of.herokuapp.com/users/sign_in 
+    mail(to: @user.email, subject: 'Welcome to The Power Of!')
+  end
 end
