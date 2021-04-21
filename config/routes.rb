fdd0495eddc369e0e322541be8e02c8f86d06478
Tarun Mookhey <tarun@pacificasearch.com>
2021-04-22 00:54:55 +0530

    about page is the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#about'
   get 'questionnaires', to: 'questionnaires#new', as: "new_questionnaire"
   post 'questionnaires', to: 'answers#create'
   get 'questions', to: 'questions#index'
